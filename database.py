1878eb42a05c4bff87ab1ab9d38dc88781998f96
matuteiglesias <matuteiglesias@gmail.com>
2024-02-01 02:33:53 -0300

    Database Transition to Google Cloud SQL

--- database.py
+++ database.py
@@ -1,17 +1,29 @@
-'''
-This file defines the Database class.
-'''
-import sqlite3
+import pymysql
+import os
+
 class Database:
     def __init__(self):
-        self.connection = sqlite3.connect("interactions.db")
+        # Example environment variables for database connection details
+        db_host = os.environ.get('DB_HOST')
+        db_user = os.environ.get('DB_USER')
+        db_password = os.environ.get('DB_PASSWORD')
+        db_name = os.environ.get('DB_NAME')
+
+        self.connection = pymysql.connect(host=db_host,
+                                          user=db_user,
+                                          password=db_password,
+                                          database=db_name,
+                                          cursorclass=pymysql.cursors.DictCursor)
         self.cursor = self.connection.cursor()
         self.create_table()
+
     def create_table(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS interactions (response TEXT)")
+
     def save_interaction(self, response):
-        self.cursor.execute("INSERT INTO interactions VALUES (?)", (response,))
+        self.cursor.execute("INSERT INTO interactions (response) VALUES (%s)", (response,))
         self.connection.commit()
+
     def close(self):
         self.cursor.close()
         self.connection.close()
