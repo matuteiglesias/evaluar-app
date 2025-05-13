1db9904556df436dae2a16dcd7fd96e61c5b70b8
matuteiglesias <matuteiglesias@gmail.com>
2025-05-13 17:45:12 -0300

    fix

--- services/firebase.py
+++ services/firebase.py
@@ -2,15 +2,17 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-_app = None
 _db = None
 
 def get_db():
-    global _app, _db
-    if not _app:
+    global _db
+
+    if not firebase_admin._apps:
         cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "env/evaluar-app-firebase-adminsdk-mvow6-456d541606.json")
         cred = credentials.Certificate(cred_path)
-        _app = firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred)
+
     if not _db:
         _db = firestore.client()
+
     return _db
